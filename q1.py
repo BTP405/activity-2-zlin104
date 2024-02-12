214696fe9ebc0bf2fbade5b49dc89a3e249df920
zlin104 <158625381+zlin104@users.noreply.github.com>
2024-02-12 13:05:56 -0500

    Add files via upload

--- q1.py
+++ q1.py
@@ -1,1 +1,19 @@
 def getPrimeNumbers(n):
+    oldList = []
+    for i in range(2, n+1):
+        oldList.append(i)
+
+    print(oldList)
+    newlist = [x for x in oldList if isPrime(x)]
+    print(newlist)
+    return newlist
+        
+
+def isPrime(n):
+    devisible = 0
+    for i in range(2, n):
+        if n%i == 0:
+            devisible+=1
+    return True if devisible == 0 else False 
+
+getPrimeNumbers(4)
